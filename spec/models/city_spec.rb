84bda6173a8dd21f3dea48a0dff8d3c8b9821028
liyijie <liyijie825@gmail.com>
2014-11-30 15:10:31 +0800

    add city annotate

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: cities
+#
+#  id         :integer          not null, primary key
+#  provice    :string(255)
+#  name       :string(255)
+#  desc       :text
+#  position   :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'rails_helper'
 
 RSpec.describe City, :type => :model do
