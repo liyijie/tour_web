c17466134c1b015aa47bc98b86f022809da15d06
liyijie <liyijie825@gmail.com>
2014-12-09 14:36:26 +0800

    1. add hotel images 2. add city hotel relation

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,3 +1,7 @@
 class Hotel < ActiveRecord::Base
   belongs_to :city
+
+  has_one :cover_image, -> { where photo_type: "cover" }, class_name: "Image", as: :imageable, dependent: :destroy
+  has_many :info_images, -> { where photo_type: "info" }, class_name: "Image", as: :imageable, dependent: :destroy
+  
 end
