a69f1345e166f6cf33a690e23a52dbb96d463045
liyijie <liyijie825@gmail.com>
2014-12-14 12:00:00 +0800

    user phone as the authenticate key, for #57

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,5 +23,31 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :authentication_keys => [:phone]
+
+  validates_uniqueness_of :phone
+  validates_presence_of :phone
+
+  before_save :ensure_authentication_token
+
+  # user phone as the authentication key, so email is not required default
+  def email_required?
+    false
+  end
+
+  def ensure_authentication_token
+    if authentication_token.blank?
+      self.authentication_token = generate_authentication_token
+    end
+  end
+  
+  private
+  
+  def generate_authentication_token
+    loop do
+      token = Devise.friendly_token
+      break token unless User.where(authentication_token: token).first
+    end
+  end
 end
