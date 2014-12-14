4444f4a85f5601deb00bcd9bfa6f23793340ae00
liyijie <liyijie825@gmail.com>
2014-12-14 21:19:14 +0800

    user scaffold_controller instead

--- spec/views/tour_orders/show.html.erb_spec.rb
+++ spec/views/tour_orders/show.html.erb_spec.rb
@@ -2,23 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "tour_orders/show", :type => :view do
   before(:each) do
-    @tour_order = assign(:tour_order, TourOrder.create!(
-      :total_price => 1.5,
-      :state => "State",
-      :number => 1,
-      :user => nil,
-      :ticket => nil,
-      :token => "Token"
-    ))
+    @tour_order = assign(:tour_order, TourOrder.create!())
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/1.5/)
-    expect(rendered).to match(/State/)
-    expect(rendered).to match(/1/)
-    expect(rendered).to match(//)
-    expect(rendered).to match(//)
-    expect(rendered).to match(/Token/)
   end
 end
