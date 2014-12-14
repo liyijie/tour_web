5786565805d6684d506b4d69b4a5c0528b217430
liyijie <liyijie825@gmail.com>
2014-12-14 12:27:33 +0800

    registration source format

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -7,16 +7,16 @@ class Api::RegistrationsController < Devise::RegistrationsController
     # resource.skip_confirmation!
     if resource.save
       sign_in resource
-        render :status => 200,
-          :json => { :success => true,
-          :info => "Registered",
-          :data => { :user => resource,
-           :auth_token => current_user.authentication_token } }
-     else
-          render :status => :unprocessable_entity,
-            :json => { :success => false,
-            :info => resource.errors,
-            :data => {} }
+      render :status => 200,
+        :json => { :success => true,
+        :info => "Registered",
+        :data => { :user => resource,
+         :auth_token => current_user.authentication_token } }
+    else
+      render :status => :unprocessable_entity,
+        :json => { :success => false,
+        :info => resource.errors,
+        :data => {} }
     end
   end
 end
