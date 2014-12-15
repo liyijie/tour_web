0ea2397f7ff91bcd1ee06a630c3b186e40754f5f
liyijie <liyijie825@gmail.com>
2014-12-15 20:42:38 +0800

    add alipay function

---
+++ app/controllers/concerns/alipay_generatable.rb
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+module AlipayGeneratable
+  extend ActiveSupport::Concern
+
+  included do
+    hide_action :generate_pay_link_by_order if respond_to?(:hide_action)
+    helper_method :generate_pay_link_by_order
+  end
+
+  def generate_pay_link_by_order(order)
+    options = {
+        :out_trade_no      => order.token,
+        :subject           => "#{order.ticket.title} 门票",
+        :logistics_type    => 'DIRECT',
+        :logistics_fee     => '0',
+        :logistics_payment => 'SELLER_PAY',
+        :price             => order.ticket.price,
+        :quantity          => order.number,
+        :discount          => 0,
+        :return_url        => alipay_done_tour_order_url(order), # localhost isn't work http://bit.ly/1cwKbsw
+        :notify_url        => alipay_notify_tour_order_url(order)
+    }
+    Alipay::Service.create_direct_pay_by_user_url(options)
+  end
+
+  def generate_wap_pay_link_by_order order
+    options = {
+      :req_data => {
+        :out_trade_no  =>  order.token,
+        :subject       => "#{order.ticket.title} 门票",
+        :total_fee     => order.total_price,
+        :notify_url    => alipay_notify_tour_order_url(order),
+        :call_back_url => tour_order_url(order)
+      }
+    }
+    token = Alipay::Service::Wap.trade_create_direct_token(options)
+    Alipay::Service::Wap.auth_and_execute(request_token: token)
+  end
+end
---
+++ config/initializers/alipay.rb
@@ -0,0 +1,7 @@
+# TODO should be load from setting files
+# Alipay.pid = '2088102065712521'
+# Alipay.key = '48p6a7j664acjcqgrrwtf5rrhxbwjl8u'
+# Alipay.seller_email = 'liyijie825@gmail.com'
+Alipay.pid = '2088611279948645'
+Alipay.key = '20t8e1k6aa4x2baimg44b5ou262x4oxd'
+Alipay.seller_email = 'gzdsj@dsjtour.com'
