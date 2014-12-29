7dd59b46d235675375a15905028cfa562ab1d2c9
liyijie <liyijie825@gmail.com>
2014-12-29 17:39:43 +0800

    add shop and dish relation

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -17,6 +17,7 @@
 class Shop < ActiveRecord::Base
 
   belongs_to :city
+  has_many :dishes
 
   has_one :cover_image, -> { where photo_type: "cover" }, class_name: "Image", as: :imageable, dependent: :destroy
   has_many :info_images, -> { where photo_type: "info" }, class_name: "Image", as: :imageable, dependent: :destroy
