a3e0b1c4b196ffcb58441d5933b4dcf74ba9cd4f
liyijie <liyijie825@gmail.com>
2014-12-30 14:55:47 +0800

    update city weather info

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -1,18 +1,19 @@
 class Weather
   
-  attr_reader :forecast_dates, :forecast_days, :forecast_icons, :forcast_temps, :forcast_tips, 
-    :weather_date, :weather_time, :weather_icon, :weather_degree, :weather_detail
+  attr_reader :forecast_dates, :forecast_days, :forecast_icons, :forecast_times, :forcast_temps, :forcast_tips, 
+    :weather_city, :weather_date, :weather_time, :weather_icon, :weather_degree, :weather_detail
 
   def initialize(city)
     city_map = {
       "贵阳" => "guiyang",
       "铜仁" => "guizhou_tongren",
       "遵义" => "zunyi",
-      "凯里" => "kaili",
+      "黔东南州" => "kaili",
       "安顺" => "anshun",
-      "都匀" => "duyun",
+      "黔南州" => "duyun",
       "毕节" => "bijie",
-      "兴义" => "xingyi"
+      "黔西南州" => "xingyi"
+      "六盘水" => "guiyang"
     }
     @city_code = city_map[city.name]
   end
@@ -25,11 +26,11 @@ class Weather
       }
     }
     response = HTTParty.get(url, params)
+
     @forecast_dates = Nokogiri::HTML(response.body).xpath("//*[@class='wt_fc_c0_i_date']").map(&:text)
     @forecast_days = Nokogiri::HTML(response.body).xpath("//*[contains(concat(' ', @class, ' '), ' wt_fc_c0_i_day ')]").map(&:text)
-    @forecast_icons = Nokogiri::HTML(response.body).xpath("//*[@class='icons0_wt png24']").map do |node|
-      node.get_attribute "src"
-    end
+    @forecast_icons = Nokogiri::HTML(response.body).xpath("//*[contains(concat(' ', @class, ' '), 'wt_fc_c0_i_icons')]")
+    @forecast_times = Nokogiri::HTML(response.body).xpath("//*[@class='wt_fc_c0_i_times']").map(&:text)
     @forcast_temps = Nokogiri::HTML(response.body).xpath("//*[@class='wt_fc_c0_i_temp']").map(&:text)
     @forcast_tips = Nokogiri::HTML(response.body).xpath("//*[@class='wt_fc_c0_i_tip']").map(&:text)
 
