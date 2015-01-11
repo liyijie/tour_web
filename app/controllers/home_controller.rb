ba90701275634528c947110aa6830e475e7f18ac
liyijie <liyijie825@gmail.com>
2015-01-11 18:41:02 +0800

    find last tours

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @categories = Category.all
     @cities = City.all
-    @tours = Tour.limit(7)
+    @tours = Tour.last(7)
     @hotels = Hotel.limit(6)
   end
 end
